70eca938c3d3cb8607df37e4f447f47e3a8ba550
FangyuanYu <fangyuan.yu@gmail.com>
2024-09-05 10:58:14 +0800

    minimal implementation doesn't need res block

--- llava/model/multimodal_projector/builder.py
+++ llava/model/multimodal_projector/builder.py
@@ -16,17 +16,17 @@ class IdentityMap(nn.Module):
         return {"mm_projector_type": "identity"}
     
 
-
-class SimpleResBlock(nn.Module): # This residual connection (with an extra projection) is another component in the multimodal projector (why not put them together?)
-    def __init__(self, channels):
-        super().__init__()
-        self.pre_norm = nn.LayerNorm(channels)
+# # This one is not required for MVP ver. implementation
+# class SimpleResBlock(nn.Module): # This residual connection (with an extra projection) is another component in the multimodal projector (why not put them together?)
+#     def __init__(self, channels):
+#         super().__init__()
+#         self.pre_norm = nn.LayerNorm(channels)
         
-        self.proj = nn.Sequential(nn.Linear(channels, channels), nn.GELU(), nn.Linear(channels, channels))
+#         self.proj = nn.Sequential(nn.Linear(channels, channels), nn.GELU(), nn.Linear(channels, channels))
 
-    def forward(self, x):
-        x = self.pre_norm(x)
-        return x + self.proj(x)
+#     def forward(self, x):
+#         x = self.pre_norm(x)
+#         return x + self.proj(x)
     
 
 def build_vision_projector(config, delay_load=False, **kwargs):
