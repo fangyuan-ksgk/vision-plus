634e434adf57f74d160c1d45f930919e15286e3b
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2024-09-16 16:08:00 +0800

    update forward input

--- llava/model/llava_llama.py
+++ llava/model/llava_llama.py
@@ -151,7 +151,7 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
         """
 
         if inputs_embeds is None:
-            (input_ids, position_ids, attention_mask, past_key_values, inputs_embeds, labels) = self.prepare_inputs_labels_for_multimodal(input_ids, position_ids, attention_mask, past_key_values, labels, images)
+            (input_ids, position_ids, attention_mask, past_key_values, inputs_embeds, labels) = self.prepare_inputs_labels_for_multimodal(input_ids, position_ids, attention_mask, past_key_values, labels, images, modalities)
 
         if dpo_forward:
             outputs = self.model(
